066e9fa25d7c9b8a1e64dac108d4978e9defa4af
dimabakuradze <xatunan12@gmail.com>
2024-01-09 19:52:13 +0400

    commit

---
+++ day-1/lesson1.py
@@ -0,0 +1,33 @@
+name = "dima"
+#name არის ვცლადი
+# = არის ვცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#  "dima" არის ცვლადისთვის მნიშვნელობა 
+
+surname = "bakuradze"
+
+#print(name)
+#პრინტ ფუქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "dima" #ეს არის str (string) ტიპის ვცვლადი
+age = 15 # ეს არის int ( intenger ) მთელი რიცხვი
+heigh = 175.5 #ეს არის float ტიპის ვცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True #true ან false
+is_ugly = False  #snackecase( უბრალოდ წერის სტილის სტანდარტულად )
+ 
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(heigh))
+#print(type(knows_programing))
+
+print(str(age)+ " წლის ")
