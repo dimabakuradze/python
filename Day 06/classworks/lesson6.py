42671caf2e4f4a9147fc127aedfd7b8f060c7c19
dimabakuradze <xatunan12@gmail.com>
2024-02-22 21:38:07 +0400

    Goa-academy

---
+++ Day 06/classworks/lesson6.py
@@ -0,0 +1,5 @@
+name = input("what is your name: ")
+lastname = input("what is your lastname: ")
+Age = int(input("what is your age: "))
+
+print(f"hello i am {name} my last name is {lastname} and my age is {Age} ")
