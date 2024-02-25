0f7c9f9014d88c17373d31c82e24ab5cdcdbf557
dimabakuradze <xatunan12@gmail.com>
2024-02-26 00:57:52 +0400

    Goa-academy

---
+++ Day 07/homeworks/lesson7.py
@@ -0,0 +1,18 @@
+# for i in range(0,10,2):
+#     print(i)
+
+
+# i = 1
+# while i<=20:
+#      print(i)
+#      i += 1
+
+
+# num = 0
+# for num in range(6):
+#     num += num
+# print(num)
+
+
+# for num in range(4):
+#     print("dima bakuradze",num)
