0a13f823fc8a3c79b8f5ea04cad868d93e121ade
dimabakuradze <xatunan12@gmail.com>
2024-02-29 21:31:27 +0400

    Goa-academy

---
+++ Day 08/homeworks/lesson8.py
@@ -0,0 +1,18 @@
+name = bool(input("enter your name: "))
+password =bool(input("enter your password: ")) 
+repeat_password =input("repeat your password: ") 
+if name <= True:
+    print("your name is correct")
+
+else: print("your name is not correct")   
+
+if password <= True:
+    print("your password is correct")
+
+
+else: print("not correct password try again")
+
+
+
+
+
