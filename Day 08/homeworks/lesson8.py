a9c30de7b7e74d3e9b8af6906551f224dc16e2d3
dimabakuradze <xatunan12@gmail.com>
2024-03-01 01:50:48 +0400

    Goa-academy

--- Day 08/homeworks/lesson8.py
+++ Day 08/homeworks/lesson8.py
@@ -1,16 +1,32 @@
-name = bool(input("enter your name: "))
-password =bool(input("enter your password: ")) 
-repeat_password =input("repeat your password: ") 
-if name <= True:
-    print("your name is correct")
+Email = input("Enter your email: ")
+password = input("Enter your password: ")
+Age = int(input("Enter your age: "))
+email_password = ("you succesfuly joined your account")
 
-else: print("your name is not correct")   
+if Email == "xatunan12@gmail.com":
+ print("this email is correct")
 
-if password <= True:
-    print("your password is correct")
+else:
+  print("your email is not correct")
+
+if password == "daduka123":
+  print("password is correct")
+  print(email_password)
+
+else:
+  print("your password is not correct")
+
+if Age >= 50:
+  print("you are old")
+
+elif  Age >= 18:
+  print("your an a adult")
+
+if Age >= 15:
+  print("your not an adult")
 
 
-else: print("not correct password try again")
+
 
 
 
