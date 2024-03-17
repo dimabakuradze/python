ed47693cba1b8afb4f470b7127ed317b9b9ec3e7
dimabakuradze <xatunan12@gmail.com>
2024-03-18 00:22:39 +0400

    Goa-academy

---
+++ Day 13/classworks/1lesson13.py
@@ -0,0 +1,12 @@
+
+my_list = []
+
+my_list.append(input("შეიტანეთ პირველი ელემენტი: "))
+my_list.append(input("შეიტანეთ მეორე ელემენტი: "))
+my_list.append(input("შეიტანეთ მესამე ელემენტი: "))
+my_list.append(input("შეიტანეთ მეოთხე ელემენტი: "))
+my_list.append(input("შეიტანეთ მეხუთე ელემენტი: "))
+
+print("პირველი ელემენტი:", my_list[0])
+print("მეოთხე ელემენტი:", my_list[3])
+print("შემდეგი 3-5 ელემენტი:", my_list[2:5])
